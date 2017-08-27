10be9fc952a1d8d2ab3ffa778f938c828a06b0a7
Eric Reyes <eric.reyes@datarobot.com>
2017-08-26 23:54:39 -0500

    add trips, admins and users to firebase

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
-(function () {
-    // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyDnL_Q28S278fyMs_WcvBEMsxn-7DITkTw",
-      authDomain: "list-go-22a0a.firebaseapp.com",
-      databaseURL: "https://list-go-22a0a.firebaseio.com",
-      projectId: "list-go-22a0a",
-      storageBucket: "list-go-22a0a.appspot.com",
-      messagingSenderId: "44651498422"
-    };
-    firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDnL_Q28S278fyMs_WcvBEMsxn-7DITkTw",
+  authDomain: "list-go-22a0a.firebaseapp.com",
+  databaseURL: "https://list-go-22a0a.firebaseio.com",
+  projectId: "list-go-22a0a",
+  storageBucket: "list-go-22a0a.appspot.com",
+  messagingSenderId: "44651498422"
+};
+firebase.initializeApp(config);
 
-    var my_obj = getElementById('my_obj');
-    const ref = firebase.database().ref().child('my_obj');
-    ref.on('value', snap => console.log(snap.val()));
+var my_object = document.getElementById('my_object');
+var baseRef = firebase.database().ref()
+//var adminsRef = baseRef.child('admins').child('admin');
+    //adminsRef.on('value', snap => my_object.innerText = snap.val());
 
-}());
+
+function saveAdmin() {
+    var leader_name = 'Mike';//document.getElementById("admin_name").value;
+    var tripName = 'hackaton';//document.getElementById("event_name").value;
+    var travel_code = 'HackMTY2017';//document.getElementById("trip_code").value;
+
+    var another_child = baseRef.child(travel_code);
+        another_child.update({
+            tripName: tripName,
+            administrator: leader_name
+    });
+
+}
+
+function saveTraveler() {
+    var traveler_name = 'Eric'; //document.getElementById("traveler_name").value;
+    var travel_code =  'HackMTY2017';//document.getElementById("travel_code").value;
+
+    //var travel_code = baseRef.equalTo(travel_code);
+
+    var trip_ref = baseRef.child(travel_code).child("userList");
+    trip_ref.update({
+        tripUser: traveler_name
+    });
+}
+
+function nukeDB(){
+    baseRef.remove();
+}
